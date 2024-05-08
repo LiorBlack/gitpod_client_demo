e8794bd59c41519139eb102d7daa5b4ac907a2fb
Lior <lior.nevo@blackstone.com>
2024-05-08 11:00:00 +0000

    feat(init): Added some user interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { ChangeEvent, useCallback, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [response, setResponse] = useState('response will show up here');
+  const [url, setUrl] = useState<string>('https://8080-liorblack-gitpodserverd-e5kuwszxjd6.ws.blackstone-pov.gitpod.cloud');
+  const onFetchResponse = useCallback(async () => {
+    try {
+      const response = await fetch(url ?? '');
+      setResponse(await response.text());
+    }
+    catch (exception) {
+      setResponse(`there was an exception with the request: ${exception}`)
+    }
+  }, [setResponse, url]);
+  const onChangeUrl = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value ?? '')
+  }, [setUrl]);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +24,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={url} onChange={onChangeUrl}/>
+        <button onClick={onFetchResponse}>get data from url</button>
+        <div style={{fontSize: 10, fontFamily: 'monospace'}}>
+        {response}
+        </div>
       </header>
     </div>
   );
